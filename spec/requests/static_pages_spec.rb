43002b2d82d79f6774bfc61877cd5ee098614286
Jay Chung <jaycyc@gmail.com>
2014-05-23 08:51:18 -0700

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+    subject { page }
+
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
@@ -30,6 +32,22 @@ describe "StaticPages" do
 		visit '/static_pages/help'
 		expect(page).to have_title ("Ruby on Rails Tutorial Sample App | Help")
 	end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
 		
   end
 
